567ae8ec91a04bc9251b613c898ebc16114c0e61
Alexander Holder <drowish95z@gmail.com>
2022-12-21 11:02:44 -0600

    Tested 0 & 100

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double temp = 0;
+            string input = "";
+            double far = 0;
+            double cel = 0;
+            Console.Write("Enter Temperature: ");
+            input = Console.ReadLine();
+            if (input != null && double.TryParse(input, out var placeholder) == true) temp = double.Parse(input);
+            cel = TempConverter.CelsiusToFahrenheit(temp);
+            far = TempConverter.FahrenheitToCelsius(temp);
+            Console.WriteLine($"{temp} Celsius becomes {far} Fahrenheit.");
+            Console.WriteLine($"{temp} Fahrenheit becomes {cel} Celsius.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius (double value) {
+            double temp = 32 + (value * 1.8);
+            return temp;
+        }
+        public static double CelsiusToFahrenheit(double value)
+        {
+            //double temp = (value - 32) * (5/9);
+            double temp = (value - 32) / 1.8;
+            return temp;
+        }
+    }
+}
